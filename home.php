aafde2f98a3e20f2ebf85512e9e273e85d76f929
Marilii Saar <mariliisaar@users.noreply.github.com>
2020-09-13 14:43:00 +0300

    2. tunni kodutöö

--- home.php
+++ home.php
@@ -2,20 +2,54 @@
   $username = "Marilii Saar";
   $fulltimenow = date("d.m.Y H:i:s");
   $hournow = date("H");
-  $partofday = "lihtsalt aeg";
-  if($hournow < 6) {
+  $partofday = "vaba aeg";
+  if($hournow >= 22 and $hournow < 6) {
       $partofday = "uneaeg";
-  } //enne 6
-  if($hournow >= 8 and $hournow <= 18) {
-	  $partofday = "õppimise aeg";
-  }
+  } // 22 - 6
+  if($hournow >= 6 and $hournow < 7.5) {
+    $partofday = "hommikuste ettevalmistuste aeg";
+  } // 6 - 7.30
+  if($hournow >= 7.5 and $hournow < 8) {
+    $partofday = "ühistranspordi aeg";
+  } // 7.30 - 8
+  if($hournow >= 8 and $hournow <= 16) {
+	  $partofday = "ülikoolis õppimise aeg";
+  } // 8 - 16
+  if($hournow >= 16 and $hournow < 16.5) {
+    $partofday = "ühistranspordi aeg";
+  } // 16 - 16.30
+
   
   // vaatame semestri kulgemist
   $semesterstart = new DateTime("2020-8-31");
   $semesterend = new DateTime("2020-12-13");
+  // $semestertest = new DateTime("2020-12-14"); // Katsetamiseks - üks päev peale semestri lõppu
+  // $semestertest2 = new DateTime("2020-8-30"); // Katsetamiseks - üks päev enne semestri algust
   $semesterduration = $semesterstart->diff($semesterend);
   $semesterdurationdays = $semesterduration->format("%r%a");
   $today = new DateTime("now");
+  $semesterpassed = $semesterstart->diff($today)->format("%r%a");
+  $todayday = $semesterpassed + 1;
+  $semesterpercent = $semesterpassed * 100 / $semesterdurationdays;
+  if($semesterpercent < 0) {
+    $semesterpercent = 0;
+  }
+  if($semesterpercent > 100) {
+    $semesterpercent = 100;
+  }
+  $semesterstatus = "Õppetöö käib, semestri algusest on möödunud " . $semesterpassed . " päeva - täna on semestri " . $todayday . ". päev.";
+  if($semesterpassed == 0) {
+    $semesterstatus = "Õppetöö käib, täna on semestri esimene päev.";
+  }
+  if($semesterpassed < 0) {
+    $semesterstatus = "Semester pole veel alanud.";
+  }
+  if($semesterpassed == $semesterdurationdays) {
+    $semesterstatus = "Õppetöö käib, semestri algusest on möödunud " . $semesterpassed . " päeva - täna on semestri viimane päev.";
+  }
+  if($semesterpassed > $semesterdurationdays) {
+    $semesterstatus = "Semester on läbi!";
+  }
   
   // Kodutöö - terve päev + semester (pole alanud, õppetöö käib, õppetöö läbi + % õppetööst)
 ?>
@@ -32,6 +66,8 @@
   <p>See leht on tehtud veebiprogrammeerimise kursusel 2020. aasta sügissemestril <a href="https://www.tlu.ee" target="_blank">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
   <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
   <p><?php echo "Praegu on " . $partofday . "."; ?></p>
+  <p><?php echo $semesterstatus ?><br />
+  <?php echo "(Läbitud on " . $semesterpercent . "% semestrist)"; ?></p>
 
 </body>
 </html>
