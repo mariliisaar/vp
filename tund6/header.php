4f071847d2ba82cf37cfe416613efdb1323987c4
Marilii Saar <mariliisaar@users.noreply.github.com>
2020-10-06 09:48:55 +0300

    Background and text colours

--- tund6/header.php
+++ tund6/header.php
@@ -4,6 +4,24 @@
   <meta charset="utf-8">
 
   <title>Veebiprogrammeerimine 2020</title>
+  <style>
+	<?php
+		echo "body { \n";
+		if(isset($_SESSION["userbgcolor"])) {
+			echo "\t background-color: " .$_SESSION["userbgcolor"] .";\n";
+		} 
+		else {
+			echo "\t background-color: #ffffff;\n";
+		}
+		if(isset($_SESSION["usertxtcolor"])) {
+			echo "\t color: " .$_SESSION["usertxtcolor"] .";\n";
+		} 
+		else {
+			echo "\t color: #000000;\n";
+		}
+		echo "\t}\n";
+	?>
+  </style>
 
 </head>
 <body>
