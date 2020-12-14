3461f891d522962d108f6cb6556fb685cb5b94ba
Marilii Saar <mariliisaar@users.noreply.github.com>
2020-12-14 13:40:24 +0200

    Deleted unused code from header

--- eksam/header.php
+++ eksam/header.php
@@ -9,24 +9,6 @@
 		echo $tolink;
 	}
   ?>
-  <style>
-	<?php
-		// echo "body { \n";
-		// if(isset($_SESSION["userbgcolor"])) {
-		// 	echo "\t background-color: " .$_SESSION["userbgcolor"] .";\n";
-		// } 
-		// else {
-		// 	echo "\t background-color: #ffffff;\n";
-		// }
-		// if(isset($_SESSION["usertxtcolor"])) {
-		// 	echo "\t color: " .$_SESSION["usertxtcolor"] .";\n";
-		// } 
-		// else {
-		// 	echo "\t color: #000000;\n";
-		// }
-		// echo "\t}\n";
-	?>
-  </style>
 
 </head>
 <body>
